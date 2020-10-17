b2f1d40c4ca5068c702b7504b90c334f94958526
George Niculae <george.niculae20@yahoo.com>
2020-10-17 12:10:27 +0300

    update

--- src/main/java/ro/sda/spring/boot/mvccontroller/DoctorMVCController.java
+++ src/main/java/ro/sda/spring/boot/mvccontroller/DoctorMVCController.java
@@ -3,9 +3,15 @@ package ro.sda.spring.boot.mvccontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ro.sda.spring.boot.entity.Doctor;
 import ro.sda.spring.boot.service.DoctorService;
+
+import javax.naming.Binding;
 
 @Controller
 public class DoctorMVCController {
@@ -27,7 +33,20 @@ public class DoctorMVCController {
     @GetMapping(path = "/doctor/delete/{id}")
     public String deleteDoctorById(@PathVariable("id") Long id, Model model) {
         this.doctorService.deleteDoctorById(id);
-        model.addAttribute("doctors", this.doctorService.findAllDoctors());
-        return "index";
+        //model.addAttribute("doctors", this.doctorService.findAllDoctors());
+        //redirectionare pe localhost:8888
+        return "redirect:/";
+    }
+
+    @GetMapping(path = "/signup")
+    public String showSignUpPage(Model model) {
+        model.addAttribute("doctor", new Doctor());
+        return "add-doctor";
+    }
+
+    @PostMapping(path = "/doctor/add")
+    public String addDoctor(@ModelAttribute("doctor") Doctor doctor, BindingResult result, Model model) {
+        this.doctorService.saveDoctor(doctor);
+        return "redirect:/";
     }
 }
