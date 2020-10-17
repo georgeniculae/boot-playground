ac9542e5aea822abf09bb61a99a00ffcd905842e
George Niculae <george.niculae20@yahoo.com>
2020-10-17 10:13:59 +0300

    update

--- src/main/java/ro/sda/spring/boot/mvccontroller/DoctorMVCController.java
+++ src/main/java/ro/sda/spring/boot/mvccontroller/DoctorMVCController.java
@@ -1,4 +1,18 @@
 package ro.sda.spring.boot.mvccontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ro.sda.spring.boot.service.DoctorService;
+
+@Controller
 public class DoctorMVCController {
+
+    private final DoctorService doctorService;
+
+    @Autowired
+    public DoctorMVCController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+
 }
